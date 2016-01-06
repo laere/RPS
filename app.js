073a25589c320e8f9c3c53fff9afad90d47159e1
Zack <bostianz@gmail.com>
2016-01-06 11:27:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ window.onload = function() {
       PLAYER_CHOICE = document.querySelector('#player-choice'),
       USER_SCORE = 0,
       COMPUTER_SCORE = 0,
-      LOCAL_STORAGE = [];
+      GAME_SCORE = { USER_SCORE: 0, COMPUTER_SCORE: 0}; // add pt to each per win, stringify to local Storage
+      key = 'scores';
 
   CHOICE_ROCK.addEventListener('click', USER_CHOICE, false);
   CHOICE_PAPER.addEventListener('click', USER_CHOICE, false);
@@ -26,9 +27,7 @@ window.onload = function() {
       console.log(USER_SCORE);
       console.log(COMPUTER_SCORE);
       ROCK('rock', compChoice);
-      PLAYER_CHOICE.style.color = 'green';
-      PLAYER_CHOICE.style.fontWeight = 'bold';
-      PLAYER_CHOICE.innerHTML = 'You chose ROCK!';
+      PLAYER_CHOICE_STYLE('ROCK');
       USER_SCORE_EL.innerHTML = 'Your Score : ' + USER_SCORE;
       COMP_SCORE_EL.innerHTML = 'Computer Score : ' + COMPUTER_SCORE;
     } else if (el === CHOICE_PAPER) {
@@ -37,9 +36,7 @@ window.onload = function() {
       console.log(USER_SCORE);
       console.log(COMPUTER_SCORE);
       PAPER('paper', compChoice);
-      PLAYER_CHOICE.style.color = 'green';
-      PLAYER_CHOICE.style.fontWeight = 'bold';
-      PLAYER_CHOICE.innerHTML = 'You chose PAPER!';
+      PLAYER_CHOICE_STYLE('PAPER!');
       USER_SCORE_EL.innerHTML = 'Your Score : ' + USER_SCORE;
       COMP_SCORE_EL.innerHTML = 'Computer Score : ' + COMPUTER_SCORE;
     } else if (el === CHOICE_SCISSORS) {
@@ -48,9 +45,7 @@ window.onload = function() {
       console.log(USER_SCORE);
       console.log(COMPUTER_SCORE);
       SCISSORS('scissors', compChoice);
-      PLAYER_CHOICE.style.color = 'green';
-      PLAYER_CHOICE.style.fontWeight = 'bold';
-      PLAYER_CHOICE.innerHTML = 'You chose SCISSORS!';
+      PLAYER_CHOICE_STYLE('SCISSORS!');
       USER_SCORE_EL.innerHTML = 'Your Score : ' + USER_SCORE;
       COMP_SCORE_EL.innerHTML = 'Computer Score : ' + COMPUTER_SCORE;
     }
@@ -62,12 +57,15 @@ window.onload = function() {
     console.log('COMP CHOICE number: ' + num);
     if (num === 1) {
       COMP_IMG.setAttribute('src', 'http://i.imgur.com/OHt2rjH.png');
+      COMP_IMG.style.border = '1px solid black';
       return 'rock';
     } else if (num === 2) {
       COMP_IMG.setAttribute('src', 'http://i.imgur.com/H86s3q4.png');
+      COMP_IMG.style.border = '1px solid black';
       return 'paper';
     } else if (num === 3) {
       COMP_IMG.setAttribute('src', 'http://i.imgur.com/rAkxkWc.png');
+      COMP_IMG.style.border = '1px solid black';
       return 'scissors';
     }
   }
@@ -75,43 +73,86 @@ window.onload = function() {
   // compare values of user choice and computer chocie
   function ROCK(USER_CHOICE, COMPUTER_CHOICE) {
     if (USER_CHOICE === CHOICE_ROCK.id && COMPUTER_CHOICE === 'scissors') {
+      //user wins
       USER_SCORE ++;
       WINNER_TXT.style.color = 'green';
       WINNER_TXT.innerHTML = 'YOU WIN!';
     } else if (USER_CHOICE === CHOICE_ROCK.id && COMPUTER_CHOICE === 'paper') {
+      //comp wins
       COMPUTER_SCORE ++;
       WINNER_TXT.style.color = 'red';
       WINNER_TXT.innerHTML = 'COMPUTER WINS!';
     } else if (USER_CHOICE === CHOICE_ROCK.id && COMPUTER_CHOICE === 'rock') {
+      WINNER_TXT.style.color = 'blue';
       WINNER_TXT.innerHTML = 'ITS A TIE!!!!!';
     }
   }
   function PAPER(USER_CHOICE, COMPUTER_CHOICE)  {
     //Paper
     if (USER_CHOICE === CHOICE_PAPER.id && COMPUTER_CHOICE === 'rock') {
+      //user wins
       USER_SCORE ++;
       WINNER_TXT.style.color = 'green';
       WINNER_TXT.innerHTML = 'YOU WIN!';
     } else if (USER_CHOICE === CHOICE_PAPER.id && COMPUTER_CHOICE === 'scissors') {
+      //comp wins
       COMPUTER_SCORE ++;
       WINNER_TXT.style.color = 'red';
       WINNER_TXT.innerHTML = 'COMPUTER WINS!';
     } else if (USER_CHOICE === CHOICE_PAPER.id && COMPUTER_CHOICE === 'paper') {
+      WINNER_TXT.style.color = 'blue';
       WINNER_TXT.innerHTML = 'ITS A TIE!!!!!';
     }
   }
   function SCISSORS(USER_CHOICE, COMPUTER_CHOICE) {
     //scissors
     if (USER_CHOICE === CHOICE_SCISSORS.id && COMPUTER_CHOICE === 'paper') {
+      //user wins
       USER_SCORE ++;
       WINNER_TXT.style.color = 'green';
       WINNER_TXT.innerHTML = 'YOU WIN!';
     } else if (USER_CHOICE === CHOICE_SCISSORS.id && COMPUTER_CHOICE === 'rock') {
+      //comp wins
       COMPUTER_SCORE ++;
       WINNER_TXT.style.color = 'red';
       WINNER_TXT.innerHTML = 'COMPUTER WINS!';
     } else if (USER_CHOICE === CHOICE_SCISSORS.id && COMPUTER_CHOICE === 'scissors') {
+      WINNER_TXT.style.color = 'blue';
       WINNER_TXT.innerHTML = 'ITS A TIE!!!!!';
     }
   }
+
+  function PLAYER_CHOICE_STYLE(choice) {
+    PLAYER_CHOICE.innerHTML = 'You chose: ' + choice;
+    PLAYER_CHOICE.style.fontWeight = 'bold';
+    PLAYER_CHOICE.style.backgroundColor = '#555';
+    PLAYER_CHOICE.style.color = 'white';
+    PLAYER_CHOICE.style.border = '2px solid #444';
+    PLAYER_CHOICE.style.borderRadius = '30px';
+    PLAYER_CHOICE.style.padding ='10px';
+  }
+
+  // Add Local Storage
+  // function RENDER_SCORES() {
+  //
+  // }
+  //
+  // //Store scores
+  // function STORE_SCORE(LOCAL_STORAGE, key) {
+  //   var score = JSON.stringify(LOCAL_STORAGE);
+  //   LOCAL_STORAGE.setItem(key, score);
+  // }
+  //fetch scores
+  // function fetch(key, callback) {
+  //   var LOCAL_STORAGE = JSON.pase(LOCAL_STORAGE.getItem(key));
+  //   callback(LOCAL_STORAGE);
+  // }
+  //
+  // function render(data) {
+  //   if (data !== null && data.hasOwnProperty('forEach')) {
+  //     data.forEach(function(current){
+  //       RENDER_SCORES(current);
+  //     });
+  //   }
+  // }
 };
